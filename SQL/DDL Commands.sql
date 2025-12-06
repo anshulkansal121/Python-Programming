04c6b67c2ea569da91e5fa037277d5b507419d60
Anshul Kansal <itsmeanshul121@gmail.com>
2025-12-06 17:13:02 +0530

    Create DDL Commands.sql

---
+++ SQL/DDL Commands.sql
@@ -0,0 +1,18 @@
+-- CREATE DataBase
+CREATE DATABASE IF NOT EXISTS Practice
+
+-- CREATE TABLE 
+CREATE TABLE IF NOT EXISTS users (
+  user_id INTEGER,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255)
+);
+
+
+
+
+
+
+
+
