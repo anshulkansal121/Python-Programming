b3e09bda59efcd80769f7127592de9f1e970c51d
Anshul Kansal <itsmeanshul121@gmail.com>
2025-12-17 22:42:40 +0530

    Update Subqueries.sql

--- SQL/Subqueries.sql
+++ SQL/Subqueries.sql
@@ -23,12 +23,66 @@ SELECT COUNT(*)
 FROM movies 
 WHERE score > (SELECT AVG(score) FROM movies)
 
+-- Find the highest rated movie in 2000 year
+SELECT * FROM movies WHERE score = (SELECT MAX(score)
+FROM movies 
+WHERE year = 2000) AND year  = 2000
+
+-- Find the highest rated movie among all the movies whose number of votes are > than dataset avg votes
+SELECT * FROM movies
+WHERE score = (SELECT MAX(score)
+				FROM movies
+				WHERE votes > (SELECT AVG(votes) FROM movies)); -- Nested Subquerries
+
+-- Independent Row Subqueries
+-- Find the users who never ordered
+SELECT * FROM users
+WHERE user_id NOT IN (SELECT DISTINCT(user_id) FROM orders)
+
+-- Find all the movies made by top 3 directors(in terms of total gross income)
+SELECT * 
+FROM movies
+WHERE director IN (
+SELECT director
+FROM movies
+GROUP BY director
+ORDER BY SUM(gross) DESC
+LIMIT 3
+)
+
+with top_directors as (SELECT director
+FROM movies
+GROUP BY director
+ORDER BY SUM(gross) DESC
+LIMIT 3)
+
+SELECT * FROM movies
+WHERE director in (SELECT * FROM top_directors)
 
 
+-- Independent Table Subquery
+-- Find most profitable movie of each year
+SELECT * FROM movies
+WHERE (year, gross - budget) IN (
+                            SELECT year, 
+                            MAX(gross - budget) as "Profit"
+                            FROM movies
+                            GROUP BY year
+  )
 
 
+-- Find the highest rated movie of each genre votes cutoff of 25000
+SELECT * 
+FROM movies
+WHERE (genre,score) IN
+( SELECT genre, MAX(score)
+FROM movies
+WHERE votes > 25000
+GROUP BY genre )
 
 
+-- Correlated Subquery
+-- Print those movies which have higher rating than the average rating of the gonera of which the movie belongs
 
 
-
+-- Find the most favourit food of each customer (criteria - most ordered)
