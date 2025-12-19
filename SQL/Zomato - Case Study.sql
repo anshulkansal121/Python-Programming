de464380ed905599de5b0db68fbf1e23f0bd6cdc
Anshul Kansal <itsmeanshul121@gmail.com>
2025-12-19 21:55:11 +0530

    Create Zomato - Case Study.sql

---
+++ SQL/Zomato - Case Study.sql
@@ -0,0 +1,12 @@
+-- Dataset Link - https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbmpuQnRwcW1FcHhmOWZWLTVmSkhsU21qVnMwZ3xBQ3Jtc0tuRmxNMkpRazZ1V2FGLWUxT2tIWXo4WHJXM3FhY0NSZjh3SHktU0RFc3hsa2NLdUtsMmdIN0E5MnFhNEJIU25FdHA2emktellrM2NncV9sS1I5WGlEWUhyVVVrRms2M0ROLTRMcmU1dko2Mk9yM3N3MA&q=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2F1JgNHxTixDA50W1l6pNFmHKRaX1a9QnXrpGLsJtzo6Gg%2Fedit%3Fusp%3Dsharing&v=jYwcB0KEFfY
+-- 
+
+USE Zomato;
+
+-- 
+
+
+
+
+
+
