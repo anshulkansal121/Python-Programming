1ba7830a9d11ff1374400dfd2fb284cc755c0e8f
Anshul Kansal <itsmeanshul121@gmail.com>
2025-12-20 22:13:07 +0530

    Update Zomato - Case Study.sql

--- SQL/Zomato - Case Study.sql
+++ SQL/Zomato - Case Study.sql
@@ -4,9 +4,31 @@
 USE Zomato;
 
 -- 
+-- Find Null values
+SELECT * 
+FROM orders
+WHERE restaurant_rating IS null;
 
 
+-- Find the number of order placed by each customer
+SELECT name, COUNT(*) AS "Number of Orders"
+FROM users u JOIN orders o
+ON u.user_id = o.user_id
+GROUP BY name
 
+-- Find restraunts with most number of menu items
+SELECT r_name, COUNT(menu_id) as "Menu Items"
+FROM restaurants r JOIN menu m
+ON r.r_id = m.r_id 
+GROUP BY r_name
+ORDER BY "Menu Items" DESC;
 
+-- Find Number of Votes and Average Rating of all the restaurants
+SELECT r_name, COUNT(*) AS "Number of Votes", AVG(restaurant_rating) as "Average Rating"
+FROM orders o JOIN restaurants r 
+ON o.r_id = r.r_id
+WHERE restaurant_rating IS NOT NULL
+GROUP BY r_name
 
+-- Find the food that is being sold at most number of resturants
 
